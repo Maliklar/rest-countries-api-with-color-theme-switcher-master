e8f6754edf94746c764368017a31d4f7c8ea6854
Malik Elbadri <elbadrimalik@gmail.com>
2022-07-16 10:14:17 +0300

    inital Setup

---
+++ src/components/NavBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav>
+    <h1>Where in the world?</h1>
+    <button class="theme-change">
+      <i class="fas fa-moon"></i>
+      Dark Mode
+    </button>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.theme-change {
+  padding: 0;
+  margin: 0;
+  background-color: transparent;
+  border: none;
+}
+</style>
